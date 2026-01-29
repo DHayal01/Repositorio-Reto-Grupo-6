e9f76392db359c58163f9f6bdd4b675fbe8e0f96
DHayal01 <dhayal01@educantabria.es>
2026-01-29 10:06:00 +0100

    Arreglo de Jitsi, implementación RRSS

--- Admin Web/contratar.php
+++ Admin Web/contratar.php
@@ -18,25 +18,22 @@
     <a href="soporte.html">
     <button type="button" class="soporte">Soporte</button>
     </a>
-    <img src="logo_sin_texto.png" class="logo">
+    <img src="imagenes/logo_sin_texto.png" class="logo">
     </div>
     <div class="formulario">
-    <form action="procesar_contratacion.php" method="post">
+    <form action="" method="post" class="formulario1">
     <h2 class="titulo_contrata">Formulario de Contratación</h2>
+    <p class="p">Agenda tu cita para poder contratar nuestros servicios.</p>
     <label for="nombre" class="label_contrata">Nombre Completo:</label>
-    <input type="text" id="nombre" name="nombre" class="input_contrata"><br><br>
+    <input type="text" id="nombre" name="nombre"><br><br>
     <label for="email" class="label_contrata">Correo Electrónico:</label>
-    <input type="email" id="email" name="email" class="input_contrata"><br><br>
+    <input type="email" id="email" name="email"><br><br>
     <label for="empresa" class="label_contrata">Empresa:</label>
-    <input type="text" id="empresa" name="empresa" class="input_contrata"><br><br>
-    <label for="pago" class="label_contrata">Método de pago:</label>  
-    <select id="pago" name="pago" required>
-        <option value="alojamiento">Trajeta de débito/crédito</option>
-        <option value="optimizacion">PayPal</option>
-        <option value="seguridad">Transferencia bancaria</option>
-    </select><br><br>
+    <input type="text" id="empresa" name="empresa"><br><br>
+    <label for="fecha_hora" class="label_contrata">Fecha y hora:</label>
+    <input type="datetime-local" id="fecha_hora" name="fecha_hora" class="input_contrata"><br><br>
     <label for="mensaje" class="label_contrata">Mensaje Adicional:</label><br>
-    <textarea id="mensaje" name="mensaje" class="textarea_contrata" rows="4" cols="50" placeholder="Detalla lo que necesites:"></textarea><br><br>
+    <textarea id="mensaje" name="mensaje" rows="4" cols="50" placeholder="Detalla lo que necesites:"></textarea>
     <input type="submit" value="Enviar Solicitud" class="boton_contrata">   
     </form>
     </div>
